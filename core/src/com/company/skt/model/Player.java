b586976c28fa4153aa12b8ce82be006ee295acec
perkelpit <70656013+perkelpit@users.noreply.github.com>
2022-04-04 16:21:48 +0200

    initial commit

--- core/src/com/company/skt/model/Player.java
+++ core/src/com/company/skt/model/Player.java
@@ -7,7 +7,7 @@ public class Player implements Serializable {
     private static final long serialVersionUID = 684898298341945719L;
     private String name;
     public Tally tally; // TODO what was that for ???
-    public boolean isReady;
+    private boolean isReady;
     public int connectivity;
     
     public Player(String name) {
@@ -19,5 +19,17 @@ public class Player implements Serializable {
     @Override
     public String toString() {
         return name + " | ready: " + isReady;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public boolean isReady() {
+        return isReady;
+    }
+    
+    public void setReady(boolean ready) {
+        isReady = ready;
     }
 }
